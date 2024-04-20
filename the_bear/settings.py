80db43f66c435caf7453f5816c9d2099d739869b
Jack Cavanagh <jack.m.cavanagh@hotmail.co.uk>
2024-04-20 15:45:39 +0000

    updated social links in base.html

--- the_bear/settings.py
+++ the_bear/settings.py
@@ -28,13 +28,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-jcav23-project4thebear-2ucmtcsbff5.ws-eu110.gitpod.io',
     '.herokuapp.com',
 ]
-
 
 # Application definition
 
