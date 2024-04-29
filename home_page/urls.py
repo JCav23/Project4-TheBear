00d70257c870cbbf4c0966eff4366384e033002a
Jack Cavanagh <jack.m.cavanagh@hotmail.co.uk>
2024-04-29 23:10:19 +0000

    updated urls to fix redirection error when navigating using urls

--- home_page/urls.py
+++ home_page/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', home_views.homepage, name='home'),
-    path('reservations/', booking_views.reservations, name='redirect_reservations')
+    path('reservations/', booking_views.reservations, name='redirect_reservations'),
+    path('reservations/delete/<booking_id>', booking_views.delete_reservation, name='delete'),
+    path('reservations/edit/<booking_id>', booking_views.edit_reservation, name='edit'),
 ]
